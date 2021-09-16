2cba8260b6299162a40944180c78bb406b76c184
serjio1905 <serjio.rv@gmail.com>
2021-09-16 16:15:18 +0300

    tmp

--- src/components/photoGallery.js
+++ src/components/photoGallery.js
@@ -8,30 +8,35 @@ import {getUserId} from "../utils/ctxHandlers.js";
 export const renderPhotoGallery = async (
     ctx: Context,
     userId: string,
-    fileIds: Array<string>,
+    files: Array<{
+        file_id: string,
+        file_unique_id: string,
+        file_size: number,
+        width: number,
+        height: number,
+        deleted: boolean,
+    }>,
     currentIndex: number=0,
-    removable: boolean=false
+    removable: boolean=false,
 ) => {
-    const fileId = fileIds[currentIndex];
+    const fileId = files[+currentIndex].file_id;
+    const fileUniqueId = files[+currentIndex].file_unique_id;
     const showPrevButton = currentIndex > 0;
-    const showNextButton = currentIndex < fileIds.length - 1;
+    const showNextButton = currentIndex < files.length - 1;
 
     const buttons = [];
     if (showPrevButton) buttons.push({
         title: 'Prev',
-        action: createAction(actions.photoGallery, [currentIndex - 1   ])
+        action: createAction(actions.photoGallery, [+currentIndex - 1])
     });
     if (removable) buttons.push({
         title: 'Delete',
-        action: createAction(actions.deletePhoto, [currentIndex])
+        action: createAction(actions.deletePhoto, [fileUniqueId])
     })
     if (showNextButton) buttons.push({
-        title: 'Prev',
-        action: createAction(actions.photoGallery, [currentIndex + 1])
+        title: 'Next',
+        action: createAction(actions.photoGallery, [+currentIndex + 1])
     });
-    console.log('>>>>>>>')
-    console.log(buttons)
-    console.log('>>>>>>> END')
     await renderPhoto(ctx, fileId);
     return await renderClickableList(ctx, 'Choose photo to delete', buttons)
 }
