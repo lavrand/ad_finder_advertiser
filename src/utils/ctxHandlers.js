5208aca3a17e33e5d804d490617ef3f173638d9e
serjio1905 <serjio.rv@gmail.com>
2021-09-16 14:29:01 +0300

    tmp

--- src/utils/ctxHandlers.js
+++ src/utils/ctxHandlers.js
@@ -9,4 +9,6 @@ export const getMessageChat = (ctx: Context) => getMessage(ctx)?.chat;
 export const getAction = (ctx: Context) => ctx.update?.callback_query?.data;
 export const getUserId = (ctx: Context) => getMessage(ctx)?.from?.id || getCallbackQuery(ctx)?.from?.id;
 export const getPhoto = (ctx: Context) => getMessage(ctx)?.photo;
-
+export const getMessageSenderFirstName = (ctx: Context) => getMessageSender(ctx)?.first_name;
+export const getMessageSenderUsername = (ctx: Context) => getMessageSender(ctx)?.username;
+export const getMessageSenderLang = (ctx: Context) => getMessageSender(ctx)?.language_code;
