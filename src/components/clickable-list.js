a74925625029cf2d58a7c4e87aba1c8f2a6f980b
serjio1905 <serjio.rv@gmail.com>
2021-09-30 14:51:45 +0300

    clickable-list chunked

--- src/components/clickable-list.js
+++ src/components/clickable-list.js
@@ -4,8 +4,15 @@ import {Context, Markup} from "telegraf";
 export const renderClickableList = async (
     ctx: Context,
     title: string,
-    list: Array<{title: string, action: string}>
+    list: Array<{title: string, action: string}>,
+    chunkSize: number=2,
 ) => {
     const render = list.map(item => Markup.button.callback(item.title, item.action));
-    return ctx.reply(title, Markup.inlineKeyboard(render).oneTime().resize());
+
+    const result = [];
+    for (let i = 0, j = render.length; i < j; i += chunkSize) {
+        result.push(render.slice(i, i + chunkSize));
+    }
+
+    return ctx.reply(title, Markup.inlineKeyboard(result).oneTime().resize());
 }
